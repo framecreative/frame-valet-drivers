c9384608f7b91848979cb7892e32a5582270964c
Alex James Bishop <alex@framecreative.com.au>
2024-07-09 17:27:49 +0930

    update install script to work with Laravel Herd instead of Valet

--- install.php
+++ install.php
@@ -4,24 +4,31 @@
 $home = getenv('HOME');
 $path = dirname(__FILE__);
 $driverFile = 'FrameWordPressValetDriver.php';
-$valetDriverPath = $home . DIRECTORY_SEPARATOR . '.config' . DIRECTORY_SEPARATOR . 'valet' .DIRECTORY_SEPARATOR . 'Drivers';
+$herdDriverPath = $home . DIRECTORY_SEPARATOR . 'Library' . DIRECTORY_SEPARATOR . 'Application Support' .DIRECTORY_SEPARATOR . 'Herd' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'valet' . DIRECTORY_SEPARATOR . 'Drivers' ;
 $targetDriverFullPath = $path . DIRECTORY_SEPARATOR . $driverFile;
-$valetDriverFullPath = $valetDriverPath . DIRECTORY_SEPARATOR . $driverFile;
+$herdDriverFullPath = $herdDriverPath . DIRECTORY_SEPARATOR . $driverFile;
 
-if ( file_exists( $valetDriverFullPath ) or is_link( $valetDriverFullPath ) ){
+if ( ! is_dir( $herdDriverPath ) ){
+    echo "Creating directory at " . $herdDriverPath . PHP_EOL;
+    mkdir( $herdDriverPath, 0755, true );
+} else {
+    echo "Correct folder exists at " . $herdDriverPath . PHP_EOL;
+}
+
+if ( file_exists( $herdDriverFullPath ) or is_link( $herdDriverFullPath ) ){
     echo 'Driver already exists, removing' . PHP_EOL;
-    unlink( $valetDriverFullPath );
+    unlink( $herdDriverFullPath );
 }
 
 echo 'Creating symlink to driver...' . PHP_EOL;
-symlink( $targetDriverFullPath, $valetDriverFullPath );
-$link = readlink( $valetDriverFullPath ) . PHP_EOL;
+symlink( $targetDriverFullPath, $herdDriverFullPath );
+$link = readlink( $herdDriverFullPath ) . PHP_EOL;
 if ( !$link ){
     echo "Error creating link: readlink() returned " . $link . PHP_EOL;
     return 1;
 }
 
 echo "Success! Symbolic link created" . PHP_EOL;
-echo $targetDriverFullPath . ' -> ' . $valetDriverFullPath . PHP_EOL;
+echo $targetDriverFullPath . ' -> ' . $herdDriverFullPath . PHP_EOL;
 
 echo PHP_EOL . 'Install completed. To update the driver just run "git pull origin master" in this repo';
